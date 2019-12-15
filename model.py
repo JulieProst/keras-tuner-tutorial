b795dbd11bc7019047dd84968c94fbd4711261c2
JulieProst <julie.prost95@gmail.com>
2019-12-15 17:19:40 +0100

    Add CNN model

--- model.py
+++ model.py
@@ -1,5 +1,12 @@
 from tensorflow import keras
-from tensorflow.keras import layers
+from tensorflow.keras.layers import (
+    Activation,
+    Conv2D,
+    Dense,
+    Dropout,
+    Flatten,
+    MaxPooling2D
+)
 
 NUM_CLASSES = 10
 INPUT_SHAPE = (32, 32, 3)
@@ -7,9 +14,9 @@ INPUT_SHAPE = (32, 32, 3)
 
 def build_simple_model(hp):
     model = keras.Sequential()
-    model.add(layers.Flatten(input_shape=INPUT_SHAPE))
+    model.add(Flatten(input_shape=INPUT_SHAPE))
     model.add(
-        layers.Dense(
+        Dense(
             units=hp.Int('units',
                          min_value=32,
                          max_value=512,
@@ -17,11 +24,37 @@ def build_simple_model(hp):
                          ),
             activation='relu')
     )
-    model.add(layers.Dense(NUM_CLASSES, activation='softmax'))
+    model.add(Dense(NUM_CLASSES, activation='softmax'))
     model.compile(
         optimizer=keras.optimizers.Adam(
             hp.Choice('learning_rate',
                       values=[1e-2, 1e-3, 1e-4])),
         loss='sparse_categorical_crossentropy',
-        metrics=['accuracy'])
+        metrics=['accuracy']
+    )
+    return model
+
+
+def build_cnn_model(hp):
+    model = keras.Sequential()
+    model.add(
+        Conv2D(
+            filters=32, kernel_size=(3, 3), activation='relu', input_shape=INPUT_SHAPE
+        )
+    )
+    model.add(Conv2D(filters=64, activation='relu', kernel_size=(3, 3)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(rate=0.25))
+    model.add(Flatten())
+    model.add(Dense(units=128, activation='relu'))
+    model.add(Dropout(rate=0.5))
+    model.add(Dense(NUM_CLASSES, activation='softmax'))
+
+    model.compile(
+        optimizer=keras.optimizers.Adam(
+            hp.Choice('learning_rate',
+                      values=[1e-2, 1e-3, 1e-4])),
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
     return model
