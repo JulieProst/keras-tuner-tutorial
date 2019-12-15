fd963cd28b9cab20b79486428947ad4feac872bc
JulieProst <julie.prost95@gmail.com>
2019-12-15 18:54:41 +0100

    Remove unused hypermodel

--- hypermodels.py
+++ hypermodels.py
@@ -1,6 +1,5 @@
 from tensorflow import keras
 from tensorflow.keras.layers import (
-    Activation,
     Conv2D,
     Dense,
     Dropout,
@@ -8,34 +7,6 @@ from tensorflow.keras.layers import (
     MaxPooling2D
 )
 from kerastuner import HyperModel
-
-
-class SimpleHyperModel(HyperModel):
-    def __init__(self, input_shape, num_classes):
-        self.input_shape = input_shape
-        self.num_classes = num_classes
-
-    def build(self, hp):
-        model = keras.Sequential()
-        model.add(Flatten(input_shape=self.input_shape))
-        model.add(
-            Dense(
-                units=hp.Int('units',
-                             min_value=32,
-                             max_value=512,
-                             step=32
-                             ),
-                activation='relu')
-        )
-        model.add(Dense(self.num_classes, activation='softmax'))
-        model.compile(
-            optimizer=keras.optimizers.Adam(
-                hp.Choice('learning_rate',
-                          values=[1e-2, 1e-3, 1e-4])),
-            loss='sparse_categorical_crossentropy',
-            metrics=['accuracy']
-        )
-        return model
 
 
 class CNNHyperModel(HyperModel):
