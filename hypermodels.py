476f55ffaa64183d2977235603c4748ef6137ed6
JulieProst <julie.prost95@gmail.com>
2019-12-15 17:50:16 +0100

    Add hyperparameters to CNN model

--- hypermodels.py
+++ hypermodels.py
@@ -47,21 +47,65 @@ class CNNHyperModel(HyperModel):
         model = keras.Sequential()
         model.add(
             Conv2D(
-                filters=32, kernel_size=(3, 3), activation='relu', input_shape=self.input_shape
+                filters=32,
+                kernel_size=(3, 3),
+                activation='relu',
+                input_shape=self.input_shape
             )
         )
-        model.add(Conv2D(filters=64, activation='relu', kernel_size=(3, 3)))
+        model.add(
+            Conv2D(
+                filters=hp.Choice(
+                    'num_filters',
+                    values=[32, 64],
+                    default=64,
+                ),
+                activation='relu',
+                kernel_size=(3, 3)
+            )
+        )
         model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(rate=0.25))
+        model.add(
+            Dropout(rate=hp.Float(
+                'dropout_1',
+                min_value=0.0,
+                max_value=0.9,
+                default=0.25
+            ))
+        )
         model.add(Flatten())
-        model.add(Dense(units=128, activation='relu'))
-        model.add(Dropout(rate=0.5))
+        model.add(
+            Dense(
+                units=hp.Int(
+                    'units',
+                    min_value=32,
+                    max_value=512,
+                    step=32,
+                    default=128
+                ),
+                activation='relu'
+            )
+        )
+        model.add(
+            Dropout(
+                rate=hp.Float(
+                    'dropout_2',
+                    min_value=0.0,
+                    max_value=0.9,
+                    default=0.25
+                )
+            )
+        )
         model.add(Dense(self.num_classes, activation='softmax'))
 
         model.compile(
             optimizer=keras.optimizers.Adam(
-                hp.Choice('learning_rate',
-                          values=[1e-2, 1e-3, 1e-4])),
+                hp.Choice(
+                    'learning_rate',
+                    values=[1e-2, 1e-3, 1e-4],
+                    default=1e-3
+                )
+            ),
             loss='sparse_categorical_crossentropy',
             metrics=['accuracy']
         )
